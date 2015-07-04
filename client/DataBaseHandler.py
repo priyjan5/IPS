41eec347bec67403b76ecd52b318d47a7b214771
Issa Hafiri <hafiri.issa@gmail.com>
2015-07-04 13:38:55 +0300

    in progress

--- client/DataBaseHandler.py
+++ client/DataBaseHandler.py
@@ -5,7 +5,9 @@ import sqlite3 as lite
 con = lite.connect('attack.db')
 cur = con.cursor()
 
-cur.execute('SELECT * from badip')
+cur.execute('SELECT * from malware')
 data = cur.fetchall()
 for item in data:
 	print item[1]
+	
+con.close()
