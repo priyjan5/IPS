d72e2b693d98803f852a74f2bae9315c1602a6dc
ihafiri <hafiri.issa@gmail.com>
2015-07-18 01:25:38 +0300

    Update loaderParser.py

--- config/loaderParser.py
+++ config/loaderParser.py
@@ -2,7 +2,5 @@ import ConfigParser
 
 parser = ConfigParser.ConfigParser()
 parser.read('sample.conf')
-
 attacks = parser.get('Attacks' , 'ipreputation').replace(" " , "").split(",")
-
 print int(attacks[0])
